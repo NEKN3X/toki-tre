f3bcbeb8c8279c6069d6f39b78e5b84c642a63d6
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2026-04-29 20:40:58 +0900

    テキスト数が多いときのUIの調整

--- src/components/routine/routine-dialog.tsx
+++ src/components/routine/routine-dialog.tsx
@@ -66,8 +66,10 @@ export default function SessionDialog({
           <div className="flex gap-2 select-none">
             <span className="text-4xl">{routine.icon}</span>
             <div className="flex flex-col justify-center gap-1">
-              <DialogTitle>{routine.title}</DialogTitle>
-              <DialogDescription className="overflow-hidden text-nowrap text-ellipsis">
+              <DialogTitle className="line-clamp-1 overflow-hidden text-ellipsis">
+                {routine.title}
+              </DialogTitle>
+              <DialogDescription className="line-clamp-1 overflow-hidden text-ellipsis">
                 {routine.description}
               </DialogDescription>
             </div>
