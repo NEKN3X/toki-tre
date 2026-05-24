93c1a2c44f0071dd1a17c1881a031acffb6e759c
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2026-05-24 17:09:13 +0900

    cardのアクセシビリティ向上
    キーボード操作に対応、モーダルを開く操作をボタンへ移動

--- src/components/routine/routine-card.tsx
+++ src/components/routine/routine-card.tsx
@@ -6,6 +6,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "../ui/card";
@@ -33,59 +34,64 @@ export default function RoutineCard({
   const [disabled, setDisabled] = useState(false);
 
   return (
-    <DialogTrigger asChild className="rounded-xl" onClick={onClick} disabled={disabled}>
-      <Card className="flex-col flex h-28 cursor-pointer justify-between p-4 text-left select-none hover:shadow-md focus:ring">
-        <CardHeader className="flex justify-between p-0">
-          <div className="flex items-center justify-center gap-2">
-            <span className="text-4xl">{routine.icon}</span>
-            <CardTitle className="line-clamp-2 w-full overflow-hidden text-wrap text-ellipsis">
-              {routine.title}
-            </CardTitle>
-          </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger
-              asChild
-              onClick={(e) => e.stopPropagation()}
+    <Card className="flex flex-col justify-between text-left select-none">
+      <CardHeader className="flex justify-between">
+        <div className="flex items-center justify-center gap-2">
+          <span className="text-4xl">{routine.icon}</span>
+          <CardTitle className="line-clamp-2 w-full overflow-hidden text-wrap text-ellipsis">
+            {routine.title}
+          </CardTitle>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="text-muted-foreground hover:text-foreground -mr-2 h-8 w-8"
             >
-              <Button
-                variant="ghost"
-                size="icon"
-                className="text-muted-foreground hover:text-foreground -mr-2 h-8 w-8"
-              >
-                <EllipsisVerticalIcon className="size-5" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              align="end"
-              className="w-32"
-              onClick={(e) => e.stopPropagation()}
+              <EllipsisVerticalIcon className="size-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            className="w-32"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <DropdownMenuItem
+              onClick={() => onEdit(routine)}
+              className="cursor-pointer"
             >
-              <DropdownMenuItem
-                onClick={() => onEdit(routine)}
-                className="cursor-pointer"
-              >
-                <Pencil className="mr-2 size-4" />
-                <span>編集</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                onClick={() => {
-                  setDisabled(true);
-                  onDelete(routine.id);
-                }}
-                className="text-destructive focus:text-destructive cursor-pointer"
-              >
-                <Trash2 className="mr-2 size-4" />
-                <span>削除</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </CardHeader>
-        <CardContent className="pl-1.5">
-          <CardDescription className="overflow-hidden text-nowrap text-ellipsis">
-            {routine.description}
-          </CardDescription>
-        </CardContent>
-      </Card>
-    </DialogTrigger>
+              <Pencil className="mr-2 size-4" />
+              <span>編集</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => {
+                setDisabled(true);
+                onDelete(routine.id);
+              }}
+              className="text-destructive focus:text-destructive cursor-pointer"
+            >
+              <Trash2 className="mr-2 size-4" />
+              <span>削除</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </CardHeader>
+      <CardContent>
+        <CardDescription className="overflow-hidden text-nowrap text-ellipsis">
+          {routine.description || "\u00A0"}
+        </CardDescription>
+      </CardContent>
+      <CardFooter className="flex gap-2">
+        <DialogTrigger
+          onClick={onClick}
+          disabled={disabled}
+          asChild
+          className="flex-1"
+        >
+          <Button>Start</Button>
+        </DialogTrigger>
+      </CardFooter>
+    </Card>
   );
 }
